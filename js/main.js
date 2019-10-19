784e4c22405f32022e25e5c2eb5d99567a89e803
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-10-19 11:06:59 +0000

    Added another line in main.js

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ txt += getFontInfo("Calibri", "green", 14) + "<br>";
 txt += getFontInfo("Arial") + "<br>";
 txt += getFontInfo("Roboto", "purple") + "<br>";
 txt += getFontInfo("Verdana", "blue", 12) + "<br>";
+txt += getFontInfo("Calibri", "orange", 16) + "<br>";
 
 // Display txt in the browser window
 document.getElementById("demo").innerHTML = txt;
